8d53cefd4cd7568e1d2f2c8714b3ec214a842c3e
Desupasu <zaxarovroma228@gmail.com>
2021-03-27 21:56:26 +0300

    add minify.js

--- minify.js
+++ minify.js
@@ -2,24 +2,12 @@ const fs = require('fs');
 const UglifyJS = require('uglify-js');
 const uglifycss = require('uglifycss');
 
-const options = {
-    mangle: {
-        toplevel: true,
-    },
-    compress: {
-        global_defs: {
-            "@console.log": "alert"
-        },
-        passes: 2
-    },
-};
-
 fs.readdirSync('./src/scripts').forEach(filename => {
     fs.writeFileSync(
         `./src/minscripts/${filename.slice(0,-2) + 'min.js'}`,
         UglifyJS.minify(
             fs.readFileSync(`./src/scripts/${filename}`, "utf8"),
-            options
+            {}
         ).code,
         "utf8"
     );
